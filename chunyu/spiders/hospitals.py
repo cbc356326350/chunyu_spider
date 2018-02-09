aef136772f0498ef47d5bf3a3ecb7212ca7ffafa
灰熊 <huixiong@dian.so>
2018-02-09 18:22:37 +0800

    春雨医生spider初始化

--- chunyu/spiders/hospitals.py
+++ chunyu/spiders/hospitals.py
@@ -7,6 +7,7 @@ from ..items import Hospital
 def get_hospital_id_from_url(url):
     def not_empty(s):
         return s and s.strip()
+
     return list(filter(not_empty, url.split('/')))[-1]
 
 
@@ -23,14 +24,19 @@ class HospitalsSpider(scrapy.Spider):
     def parse_city(self, response):
         city_list = response.css('.list')
         for city in city_list:
-            city_url = city.css('.hospital-name::attr(href)').extract()[0]
+            city_name = city.css('label::text').extract_first()
+            city_url = city.css('.hospital-name::attr(href)').extract_first()
             print(city_url)
-            yield Request(url=city_url, callback=self.parse_hospital)
+            yield Request(url=city_url, callback=lambda response, city_param=city_name: self.parse_hospital(response,city=city_param))
         pass
 
-    def parse_hospital(self, response):
+    def parse_hospital(self, response, city):
         hospital = Hospital()
         hospital['id'] = get_hospital_id_from_url(response.url)
-        hospital['name'] = response.css('h3.title::text').extract()
-        hospital['level'] = response.css('div.content-title').css(".label::text")[0].extract()
+        hospital['name'] = response.css('h3.title::text').extract_first()
+
+        hospital['city'] = city
+        hospital['level'] = response.css('div.content-title').css(".label::text")[0].extract_first()
+        hospital['type'] = response.css('div.content-title').css(".label::text")[1].extract_first()
+        hospital['detail'] = response.css
         return hospital
