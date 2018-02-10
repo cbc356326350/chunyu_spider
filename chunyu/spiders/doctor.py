4f438421f605a743c32776084361c67fdd1be94f
caicai <356326350@qq.com>
2018-02-10 23:17:01 +0800

    解决医生爬虫bug

--- chunyu/spiders/doctor.py
+++ chunyu/spiders/doctor.py
@@ -37,21 +37,23 @@ class DoctorSpider(scrapy.Spider):
             yield Request(url=url, callback=self.parse_hospital_doctor)
 
     def parse_hospital_doctor(self, response):
-        for doctor_div in response.css('dev.doctor-list clearfix').css('div'):
-            url = self.chunyu_domain + doctor_div.css('a::attr(href)').extract_first()
-            skills = doctor_div.css('p.class::text').extract_first().split('：')[1].split('、')
+        for doctor_div in response.css('div.doctor-info-item'):
+            url = self.chunyu_domain + doctor_div.css('a.name-wrap::attr(href)').extract_first()
+            skills = doctor_div.css('p.des::text').extract_first().split('：')[1].split('、')
             yield Request(url=url, callback=lambda response, skills_param=skills: self.parse_doctor(response,
                                                                                                     skills=skills_param))
-        next = self.chunyu_domain + response.css('dev.pagebar').css('a.next::attr(href)')
-        yield Request(url=next, callback=self.parse_hospital_doctor)
+        next_str = response.css('a.next::attr(href)').extract_first()
+        if 'javascript' not in next_str:
+            next = self.chunyu_domain + next_str
+            yield Request(url=next, callback=self.parse_hospital_doctor)
 
     def parse_doctor(self, response, skills):
         doctor = Doctor()
-        doctor_head_div = response.css('div.doctor-wrap clearfix')
+        doctor_head_div = response.css('div.doctor-wrap')
         doctor['skills'] = skills
         doctor['avatar'] = doctor_head_div.css('div.avatar-wrap').css('img::attr(src)').extract_first()
         doctor['name'] = doctor_head_div.css('span.name::text').extract_first().strip()
-        doctor['department'] = doctor_head_div.css('a.clinic::text(href)').extract_first().strip()
+        doctor['department'] = doctor_head_div.css('a.clinic::text').extract_first().strip()
         doctor['level'] = doctor_head_div.css('span.grade::text').extract_first().strip()
         doctor['hospital'] = doctor_head_div.css('a.hospital::text').extract_first().strip()
         doctor['hospital_id'] = get_hospital_id_from_url(doctor_head_div.css('a.hospital::attr(href)').extract_first())
@@ -67,6 +69,6 @@ class DoctorSpider(scrapy.Spider):
             doctor_head_div.css('ul.doctor-data').css('li')[3].css('span.number::text').extract_first())
         doctor['background'] = get_detail(response.css('p.detail')[0].css('::text').extract())
         doctor['skills_detail'] = get_detail(response.css('p.detail')[1].css('::text').extract())
-        doctor['patient_judge'] = list(map(lambda s: s.strip(), response.css('ul.tags').css('li::text').extract()))
-        doctor['favourable_questions'] = response.css('li.tag-item::attr(data-keywords)').extract()
+        doctor['patient_judge'] = list(filter_empty(list(map(lambda s: s.strip(), response.css('li.tag-item-dead::text').extract()))))
+        doctor['favourable_questions'] = response.css('#hot-qa-tags').css('.tag-item::attr(data-keywords)').extract()
         return doctor
